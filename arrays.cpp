4fcc5923c40107da40e656275208a98a4b2cf0e9
salvatore <salvatore@SALVATORE.SALVATORE>
2025-01-29 03:49:40 +0400

    Update

--- arrays.cpp
+++ arrays.cpp
@@ -77,19 +77,24 @@ int main(){
     // A real life example : To demonstrate a practical example of using arrays, let's create a program that calculates the average of different ages:
 
     // An array storing different ages
-    int ages[5] = {17,169,170,171,20};
-    float avg,sum = 0;
-    int i;
-    // Get the length of the array
-    int length = sizeof(ages) / sizeof(ages[0]);
+    // int ages[5] = {17,169,170,171,20};
+    // float avg,sum = 0;
+    // int i;
+    // // Get the length of the array
+    // int length = sizeof(ages) / sizeof(ages[0]);
     
-    // Loop through the elements of the array
-    for(int age : ages){
-        sum += age;
-    }
+    // // Loop through the elements of the array
+    // for(int age : ages){
+    //     sum += age;
+    // }
 
-    avg = sum / length;
-    cout << "THe average is: " << avg << endl;
+    // avg = sum / length;
+    // cout << "THe average is: " << avg << endl;
+
+
+
+
+    
 
     return 0;
 }
---
+++ average_number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+//Average number calculator;
+
+int numbers[5] = {17,20,170,171,172};
+float avg, sum = 0;
+int length;
+length = sizeof(numbers) / sizeof(numbers[0]);
+for(int number : numbers){
+    sum += number;
+}
+
+avg = sum / length;
+
+cout << "Average number is: " << avg << endl;
+
+
+
+
+}
