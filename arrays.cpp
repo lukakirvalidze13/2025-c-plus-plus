7f72c71ecb91e70810e10567a0ba376ba95aa209
salvatore <salvatore@SALVATORE.SALVATORE>
2025-01-29 02:45:10 +0400

    Done

---
+++ arrays.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // int model[4];
+    // model[0] = 3;
+    // model[1] = 4;
+    // model[2] = 8;
+    // model[3] = 9;
+    // for(int i = 0; i <= 3; i++){
+    //     cout << model[i] << endl;
+    // }
+
+
+    // string name[4] = {"BMW", "RANGE ROVER", "TOYOTA", "PORSCHE"};
+    // for(int i = 3; i >= 0; i--){
+    //     cout << name[i]<< endl;
+    // }
+
+
+    // string my_love[2] = {"ME", "HER"};
+    // for(int i = 0; i < 2; i++){
+    //     cout << my_love[i] << endl;
+    // }
+    
+
+    // string love[4] = {"ME", "ME", "HER", "HER"};
+    // for(string i:love){
+    //     cout << i << endl;
+    // }
+
+    // string love[2] = {"LUKA", "ANNA"};
+    // for(string x : love){
+    //     cout << x << endl;
+    // }
+
+
+    string car[] = {"BMW"};
+    for(int i = 0; i<1; i++){
+        cout << car[i];
+    }
+
+
+    return 0;
+}
+
+
