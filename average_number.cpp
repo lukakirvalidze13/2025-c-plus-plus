e3e65419a82dc51f79c11fe0f392dde3a8ff7559
salvatore <salvatore@SALVATORE.SALVATORE>
2025-01-29 03:57:03 +0400

    Update

--- average_number.cpp
+++ average_number.cpp
@@ -9,12 +9,18 @@ int numbers[5] = {17,20,170,171,172};
 float avg, sum = 0;
 int length;
 length = sizeof(numbers) / sizeof(numbers[0]);
-for(int number : numbers){
-    sum = sum + number;
+// for(int number : numbers){
+//     sum = sum + number;
+// }
+
+for(int i = 0; i <= 4; i++){
+    sum = sum + numbers[i];
 }
 
-avg = sum / length;
 
+avg = sum / length;
+cout << sum << endl;
+cout << length << endl;
 cout << "Average number is: " << avg << endl;
 
 
