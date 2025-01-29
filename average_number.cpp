fa88405c249b2e880db9ded099cccee260f071b9
salvatore <salvatore@SALVATORE.SALVATORE>
2025-01-30 03:36:36 +0400

    Update

--- average_number.cpp
+++ average_number.cpp
@@ -5,23 +5,26 @@ int main(){
 
 //Average number calculator;
 
-int numbers[5] = {17,20,170,171,172};
-float avg, sum = 0;
-int length;
-length = sizeof(numbers) / sizeof(numbers[0]);
-// for(int number : numbers){
-//     sum = sum + number;
+// int numbers[5] = {17,20,170,171,172};
+// float avg, sum = 0;
+// int length;
+// length = sizeof(numbers) / sizeof(numbers[0]);
+// // for(int number : numbers){
+// //     sum = sum + number;
+// // }
+
+// for(int i = 0; i <= 4; i++){
+//     sum = sum + numbers[i];
 // }
 
-for(int i = 0; i <= 4; i++){
-    sum = sum + numbers[i];
-}
+
+// avg = sum / length;
+// cout << sum << endl;
+// cout << length << endl;
+// cout << "Average number is: " << avg << endl;
 
 
-avg = sum / length;
-cout << sum << endl;
-cout << length << endl;
-cout << "Average number is: " << avg << endl;
+
 
 
 
--- lowest_number.cpp
+++ lowest_number.cpp
@@ -4,19 +4,37 @@ int main(){
 
     //Lowest number
 
-    int numbers [5] = {1,2,3,20,17}; // array storing different numbers
+    // int numbers [5] = {1,2,3,20,17}; // array storing different numbers
 
 
-    int length = sizeof(numbers) / sizeof(numbers[0]); // length of the array
-    int lowest_number = numbers[0];
+    // int length = sizeof(numbers) / sizeof(numbers[0]); // length of the array
+    // int lowest_number = numbers[0];
     
-    for(int number : numbers){ // Loop through the elements of the numbers array to find the lowest number
-        if(lowest_number > number){
-            lowest_number = number;
+    // for(int number : numbers){ // Loop through the elements of the numbers array to find the lowest number
+    //     if(lowest_number > number){
+    //         lowest_number = number;
+    //     }
+    // }
+    // cout << "The lowest number is: " << lowest_number<<endl; //printing the lowest number
+
+
+
+
+    //SECOND ANSWER
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for ( int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    int lowest = arr[0];
+    for(int i : arr){
+        if(lowest > i){
+            lowest = i;
         }
     }
-    cout << "The lowest number is: " << lowest_number<<endl; //printing the lowest number
-
+    cout << lowest;
 
     return 0;
 }
