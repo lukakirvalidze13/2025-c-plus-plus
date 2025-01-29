d20af48e44928e3f9c1f94c393e4db55983b82af
salvatore <salvatore@SALVATORE.SALVATORE>
2025-01-29 04:34:04 +0400

    Update

--- lowest_number.cpp
+++ lowest_number.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 int main(){
 
-    int numbers [5] = {1,2,3,20,17};
-    int i;
+    //Lowest number
+
+    int numbers [5] = {1,2,3,20,17}; // array storing different numbers
+
 
     int length = sizeof(numbers) / sizeof(numbers[0]); // length of the array
     int lowest_number = numbers[0];
     
-    for(int number : numbers){
+    for(int number : numbers){ // Loop through the elements of the numbers array to find the lowest number
         if(lowest_number > number){
             lowest_number = number;
         }
     }
-    cout << "The lowest number is: " << lowest_number<<endl;
+    cout << "The lowest number is: " << lowest_number<<endl; //printing the lowest number
 
 
     return 0;
